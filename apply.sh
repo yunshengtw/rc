e1da60128eceb0fecd56627290b7c5db8e0d251e
Yun-Sheng Chang <yschang@mit.edu>
2025-03-19 16:28:32 -0400

    Add alacritty, tmux, and zsh

--- apply.sh
+++ apply.sh
@@ -1,7 +1,25 @@
-cp bashrc ~/.bashrc
-cp bash_profile ~/.bash_profile
-cp bashrc-local ~/.bashrc-local
-cp vimrc ~/.vimrc
-cp screenrc ~/.screenrc
+#!/bin/bash
+
+# cp bashrc ~/.bashrc
+# cp bash_profile ~/.bash_profile
+# cp bashrc-local ~/.bashrc-local
+# cp vimrc ~/.vimrc
+# cp screenrc ~/.screenrc
+
+# emacs
 cp emacs ~/.emacs
+cp -r emacs.d ~/emacs.d
+
+# tmux
+cp tmux.conf .tmux.conf
+
+# alacritty
+mkdir -p ~/.config/alacritty
+cp alacritty.toml ~/.config/alacritty/alacritty.toml
+
+# zsh
+cp zshrc ~/.zshrc
+cp zshrc-ssh ~/.zshrc-ssh
+
+# ssh
 cp ssh-config ~/.ssh/config
